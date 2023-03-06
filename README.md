6a769feb00db6769ee5dd8175525e7223a3db73f
Rahul राहुल <rahul_yadav@external.mckinsey.com>
2023-03-06 13:12:25 +0530

    Some changes in the no of questions

---
+++ README.md
@@ -0,0 +1,11 @@
+# Scrape StackOverflow Questions
+
+This repo is made for scrapping questions based on the tags provided in the code.
+
+If you want to scrape any question related to any search query, you can change the `search_term` inside the
+code, which is located at `tests/scrape.spec.ts`.
+
+For eg - let's say I want to search for `selenium-webdriver`, then replace the term `search_term` with the 
+`selenium-webdriver` inside the `tests/scrape.spec.ts`.
+
+
--- tests/scrape.spec.ts
+++ tests/scrape.spec.ts
@@ -5,33 +5,40 @@ test.use({ viewport: { width: 1400, height: 1000 } });
 test("Launch the SO page with Playwright questions", async () => {
   await test.step("Launch the url", async () => {
     const browser = await chromium.launch({
-      headless: true,
+      headless: false,
     });
     const context = await browser.newContext();
     const page = await context.newPage();
-    const search_term = "selenium-webdriver" // add your search term here
+    const search_term = "selenium-webdriver"; // add your search term here
     const url = `https://stackoverflow.com/questions/tagged/${search_term}`;
-    
+
     await page.goto(url);
-    await page.waitForSelector(".ws-nowrap.s-btn.s-btn__primary",{
-        state: 'visible'
+    await page.waitForSelector(".ws-nowrap.s-btn.s-btn__primary", {
+      state: "visible",
     });
 
-    const total_questions  = await page.locator('#questions > div').count();
+    //change to view 50 questions
+    await page.locator("a[title='Show 50 items per page']").click();
+    await page.waitForTimeout(5000);
+    const total_questions = await page.locator("#questions > div").count();
     await console.log(`Total questions on the page ${total_questions}`);
 
-    const questions_class = await page.$$eval('.s-post-summary--content',all_items=>{
+    const questions_class = await page.$$eval(
+      ".s-post-summary--content",
+      (all_items) => {
         const data = [];
-        all_items.forEach(questions=>{
-            const question_name  = questions.querySelector('h3').innerText;
-            const question_url = "https://stackoverflow.com"+questions.querySelector('h3 > a').getAttribute('href')
+        all_items.forEach((questions) => {
+          const question_name = questions.querySelector("h3").innerText;
+          const question_url =
+            "https://stackoverflow.com" +
+            questions.querySelector("h3 > a").getAttribute("href");
 
-            data.push({question_name, question_url});
+          data.push({ question_name, question_url });
         });
         return data;
-    });
+      }
+    );
     await console.log(questions_class);
     await browser.close();
   });
-
 });

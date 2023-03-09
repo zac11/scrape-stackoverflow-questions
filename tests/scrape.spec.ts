551b77ca1551c277275d92648549c893d70a0ce6
Rahul राहुल <rahul_yadav@external.mckinsey.com>
2023-03-09 13:54:06 +0530

    Add email specific code in github actions yml

--- tests/scrape.spec.ts
+++ tests/scrape.spec.ts
@@ -9,10 +9,12 @@ test("Launch the SO page with Playwright questions", async () => {
     });
     const context = await browser.newContext();
     const page = await context.newPage();
+    const baseurl = "https://stackoverflow.com";
+
     const search_term = "selenium-webdriver"; // add your search term here
     const url = `https://stackoverflow.com/questions/tagged/${search_term}`;
-
     await page.goto(url);
+
     await page.waitForSelector(".ws-nowrap.s-btn.s-btn__primary", {
       state: "visible",
     });
@@ -20,6 +22,13 @@ test("Launch the SO page with Playwright questions", async () => {
     //change to view 50 questions
     await page.locator("a[title='Show 50 items per page']").click();
     await page.waitForTimeout(5000);
+
+/*
+    //enable this for viewing only unanswered questions
+    await page.locator("[data-nav-value='Unanswered']").click();
+    await page.waitForTimeout(5000);
+*/
+
     const total_questions = await page.locator("#questions > div").count();
     await console.log(`Total questions on the page ${total_questions}`);
 
@@ -30,7 +39,7 @@ test("Launch the SO page with Playwright questions", async () => {
         all_items.forEach((questions) => {
           const question_name = questions.querySelector("h3").innerText;
           const question_url =
-            "https://stackoverflow.com" +
+            baseurl +
             questions.querySelector("h3 > a").getAttribute("href");
 
           data.push({ question_name, question_url });
